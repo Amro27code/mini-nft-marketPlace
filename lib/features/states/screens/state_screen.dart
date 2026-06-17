d2aef234591531eda7c9fde2923500c69e7d7e22
Amro27code <amrosramrosr05@gmail.com>
2026-06-17 08:58:34 +0300

    finish app , without clean stats page

--- lib/features/states/screens/state_screen.dart
+++ lib/features/states/screens/state_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace/core/ReUseFiles/color_manager.dart';
 import 'package:mini_nft_marketplace/core/ReUseFiles/constants.dart';
 import 'package:mini_nft_marketplace/core/ReUseFiles/icons.dart';
-import 'package:mini_nft_marketplace/core/ReUseFiles/image_path_manager.dart';
 import 'package:mini_nft_marketplace/core/ReUseFiles/numbers_manager.dart';
 import 'package:mini_nft_marketplace/core/ReUseFiles/text_manager.dart';
 import 'package:mini_nft_marketplace/core/widgets/app_bar_wdg.dart';
@@ -16,331 +15,6 @@ class StatesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<ListTile> a = [
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-      ListTile(
-        leading: Text("1"),
-        horizontalTitleGap: -1,
-        title: Row(
-          spacing: 13.51,
-          children: [
-            Image(
-              image: AssetImage(ImagePathManager.hotNew2),
-              width: 39,
-              height: 39,
-            ),
-            Column(
-              crossAxisAlignment: .start,
-              children: [Text("Azumi"), Text("view info")],
-            ),
-          ],
-        ),
-        trailing: Column(
-          crossAxisAlignment: .end,
-          spacing: 7,
-          children: [Text("200055.02"), Text("3,99%")],
-        ),
-      ),
-    ];
-
     return Scaffold(
       backgroundColor: ColorManager.backgroundHomePage,
       appBar: appBar(
@@ -358,76 +32,85 @@ class StatesPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(color: ColorManager.greyColor)),
-            ),
-            child: Row(
-              mainAxisAlignment: .spaceEvenly,
-              children: [
-                topTitleWidget(
-                  text: TextManager.ranking,
-                  icon: IconsManager.probabilityBnb,
-                  isActive: true,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: ColorManager.greyColor),
                 ),
-                topTitleWidget(
-                  text: TextManager.activity,
-                  icon: IconsManager.activity,
-                  isActive: false,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 27),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Row(
-              mainAxisAlignment: .spaceBetween,
-              children: [
-                subTitleWidget(
-                  icon1: Icons.window_outlined,
-                  text: "All categories",
-                  icon2: Icons.keyboard_arrow_down,
-                ),
-                subTitleWidget(
-                  icon1: CupertinoIcons.link,
-                  text: "All Chains",
-                  icon2: Icons.keyboard_arrow_down,
-                ),
-              ],
-            ),
-          ),
-          // const SizedBox(height: 15),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 26),
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.68,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18),
-              gradient: LinearGradient(
-                begin: .topCenter,
-                end: .bottomCenter,
-                colors: [
-                  Color(0xff2C2759),
-                  Color(0xff2C2759),
-                  Color(0xff22406D),
-                  Color(0xff2C2759),
-                  Color(0xff2C2759),
-                  Color(0xff2C2759),
+              ),
+              child: Row(
+                mainAxisAlignment: .spaceEvenly,
+                children: [
+                  topTitleWidget(
+                    text: TextManager.ranking,
+                    icon: IconsManager.probabilityBnb,
+                    isActive: true,
+                  ),
+                  topTitleWidget(
+                    text: TextManager.activity,
+                    icon: IconsManager.activity,
+                    isActive: false,
+                  ),
                 ],
               ),
             ),
-            child: ListView.separated(
-              padding: const EdgeInsets.only(top: 18),
-              itemCount: Constants.tableRowStatsPage.length,
-              itemBuilder: (context, index) =>
-                  listTileWidget(tableRow: Constants.tableRowStatsPage[index]),
-              separatorBuilder: (context, index) => const SizedBox(height: 9),
+            const SizedBox(height: 27),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Row(
+                mainAxisAlignment: .spaceBetween,
+                children: [
+                  subTitleWidget(
+                    icon1: Icons.window_outlined,
+                    text: "All categories",
+                    icon2: Icons.keyboard_arrow_down,
+                  ),
+                  subTitleWidget(
+                    icon1: CupertinoIcons.link,
+                    text: "All Chains",
+                    icon2: Icons.keyboard_arrow_down,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            // const SizedBox(height: 15),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 14.0,
+                vertical: 26,
+              ),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                gradient: LinearGradient(
+                  begin: .topCenter,
+                  end: .bottomCenter,
+                  colors: [
+                    Color(0xff2C2759),
+                    Color(0xff2C2759),
+                    Color(0xff22406D),
+                    Color(0xff2C2759),
+                    Color(0xff2C2759),
+                    Color(0xff2C2759),
+                  ],
+                ),
+              ),
+              child: ListView.separated(
+                shrinkWrap: true, //بتشيلها بعطي error
+                physics: NeverScrollableScrollPhysics(), //عشان تعمل scroll على الليست فيو نفسها كمان
+                padding: const EdgeInsets.only(top: 18, bottom: 75),
+                itemCount: Constants.tableRowStatsPage.length,
+                itemBuilder: (context, index) => listTileWidget(
+                  tableRow: Constants.tableRowStatsPage[index],
+                ),
+                separatorBuilder: (context, index) => const SizedBox(height: 9),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
